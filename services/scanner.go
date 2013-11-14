36f42843b8517d0e18f712ff5da1fa4143aa30c4
Juan B. Rodriguez <jbrodriguez@apertoire.net>
2013-11-13 21:45:07 -0500

    well, it’s finding the files i want it to find. next up - read from a text file with the regular expressions - build an extension list and only run the regexps against those files that match … should make it faster, because non matching expressions will evaluate all expressions and fail at that - keep going

--- services/scanner.go
+++ services/scanner.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 )
 
 var re = regexp.MustCompile(`/Volumes/films/(?P<Resolution>.*?)/(?P<Name>.*?)/(?:.*/)*.*\.(?P<FileType>bdmv|iso|img|nrg|mkv|avi|xvid|ts|mpg|dvr-ms)$`)
@@ -15,10 +16,30 @@ var re = regexp.MustCompile(`/Volumes/films/(?P<Resolution>.*?)/(?P<Name>.*?)/(?
 type Scanner struct {
 	Bus    *bus.Bus
 	Config helper.Config
+
+	re [3]*regexp.Regexp
 }
 
 func (self *Scanner) Start() {
 	log.Printf("starting scanner service ...")
+
+	log.Printf("compiling regular expressions ...")
+
+	// test:="I am leaving from home in a while"
+	// prepositionsRegex := make([]*regexp.Regexp, len(preps))
+	// for i := 0; i < len(preps); i++ {
+	// prepositionsRegex[i]=regexp.MustCompile(`\b`+preps[i]+`\b`)
+	// }
+
+	// for i := 0; i < len(prepositionsRegex); i++ {
+	// fmt.Println(prepositionsRegex[i].String())
+	// if loc := prepositionsRegex[i].FindStringIndex(test); loc != nil{
+	// fmt.Println(test[loc[0]:loc[1]], "found at: ", loc[0])
+	// break
+
+	self.re[0] = regexp.MustCompile(`/volumes/films/(?P<Resolution>.*?)/(?P<Name>.*?)/(?:.*/)*bdmv/index.bdmv$`)
+	self.re[1] = regexp.MustCompile(`/volumes/films/(?P<Resolution>.*?)/(?P<Name>.*?)/(?:.*/)*.*\.(?P<FileType>iso|img|nrg|mkv|avi|xvid|ts|mpg|dvr-ms)$`)
+	self.re[2] = regexp.MustCompile(`/volumes/films/(?P<Resolution>.*?)/(?P<Name>.*?)/(?:.*/)*(?:video_ts|hv000i01)\.(?P<FileType>ifo)$`)
 
 	go self.react()
 
@@ -38,22 +59,24 @@ func (self *Scanner) react() {
 	}
 }
 
-func visit(path string, f os.FileInfo, err error) error {
-	log.Printf("p: %s", path)
+func (self *Scanner) visit(path string, f os.FileInfo, err error) error {
+	for i := 0; i < 3; i++ {
+		match := self.re[i].FindStringSubmatch(strings.ToLower(path))
+		if match == nil {
+			continue
+		}
 
-	match := re.FindStringSubmatch(path)
-	if len(match) != 3 {
+		log.Printf("p: %s", path)
 		return nil
 	}
 
-	log.Printf("p: %s", path)
 	return nil
 }
 
 func (self *Scanner) doMovieScan(user *model.MovieScanReq, reply chan *model.MovieScanRep) {
 	log.Printf("i got here")
 
-	err := filepath.Walk("/Volumes/films", visit)
+	err := filepath.Walk("/Volumes/films", self.visit)
 	log.Println("err: %s", err)
 
 }
